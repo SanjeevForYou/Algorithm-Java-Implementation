d2c7d82006e6c8cd6c95bcfffa0d3a18c5404ead
SanjeevForYou <yoursanjeev0@gmail.com>
2016-09-08 09:12:11 -0500

    power set problem

---
+++ Algorithm/src/algorithm/lab2/PowerSetAlgorithm.java
@@ -0,0 +1,16 @@
+package algorithm.lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PowerSetAlgorithm {
+
+	public static void main(String[] args)
+	{
+		
+	}
+	public static List powerSet(List x)
+	{
+		return new ArrayList<>();
+	}
+}
